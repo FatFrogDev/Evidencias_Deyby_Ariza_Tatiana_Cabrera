8f9abdebc50be40915b09041a456df5e20a7aad7
FatFrogDev <arizapaul987@gmail.com>
2023-03-28 16:27:06 -0500

    Commit

--- Trimestre_I/Retos_Ariza_Deyby/03_TALLER_Implementación_POO_ Java/src/Principal/Inicio_3.java
+++ Trimestre_I/Retos_Ariza_Deyby/03_TALLER_Implementación_POO_ Java/src/Principal/Inicio_3.java
@@ -3,7 +3,6 @@ import Salud.*;
 public class Inicio_3
 {       public static void main(String[] args)
         { 
-
             Empleado emp = new Empleado();
             emp.pedirDatos();
             emp.mostrarEmpleado();
