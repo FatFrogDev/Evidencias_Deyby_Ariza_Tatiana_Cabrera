dde7fda3cb72eb7940b02a69c7f1ef9217577da3
Deyby Ariza <125670549+FatFrogDev@users.noreply.github.com>
2023-06-29 03:52:33 -0500

    Update bolsillo.sql

--- Trimestre_II/retos/03_APLICACION_CRUD_MVC_MYSQL_RELACIONES/proyectoweb/bolsillo.sql
+++ Trimestre_II/retos/03_APLICACION_CRUD_MVC_MYSQL_RELACIONES/proyectoweb/bolsillo.sql
@@ -25,6 +25,7 @@ CREATE TABLE `bolsillo` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
+alter table usuario add unique (idUsuario);
 
 ALTER TABLE `bolsillo`
 ADD CONSTRAINT `FKUsuario` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`);
